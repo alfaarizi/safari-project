ffc1d19feb77e7616420235d9c60730119ffda8e
Muhammad Al Farizi <ziziyusrizal@gmail.com>
2025-04-24 11:42:50 +0200

    fix: minot tweak for incorrect attribute name

--- tests/test_gameplay_extras.py
+++ tests/test_gameplay_extras.py
@@ -21,7 +21,7 @@ def small_board() -> Board:
 # ---------------------------------------------------------------- path logic
 def test_path_is_contiguous(small_board: Board):
 
-    path: List[Vector2] = small_board.jeeps[0].path          # already centre-offset
+    path: List[Vector2] = small_board.jeeps[0]._path          # already centre-offset
     wp_cells = [Vector2(int(p.x), int(p.y)) for p in path]
 
     for a, b in itertools.pairwise(wp_cells):
