f97840d56a99d0adb1543592538d8b4899e348c8
Nikhil Gowda <Nick3344@users.noreply.github.com>
2025-05-21 01:00:29 +0200

    see failing test cases

--- tests/test_poacher_ranger.py
+++ tests/test_poacher_ranger.py
@@ -1,7 +1,11 @@
+from my_safari_project.control.wildlife_ai import WildlifeAI
+from my_safari_project.model.board import Board
+from my_safari_project.model.capital import Capital
+from pygame.math import Vector2
 
 
 def test_ranger_catches_poacher():
-   ''' board = Board(10, 10)
+    board = Board(10, 10)
     capital = Capital(1000)
     ai = WildlifeAI(board, capital)
 
@@ -10,5 +14,4 @@ def test_ranger_catches_poacher():
 
     ai.update(1.0)
     assert len(board.poachers) == 0
-    assert capital.current_balance == 1050'''
-   pass
+    assert capital.current_balance == 1050
